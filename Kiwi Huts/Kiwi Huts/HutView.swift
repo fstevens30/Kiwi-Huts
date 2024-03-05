65ea90c0b90463ff19e6b2c930883ec40b9580a0
Flynn Stevens <flynnstevens30@gmail.com>
2024-03-06 12:24:18 +1300

    Fixed the preview for HutDetail

--- Kiwi Huts/Kiwi Huts/HutView.swift
+++ Kiwi Huts/Kiwi Huts/HutView.swift
@@ -18,22 +18,23 @@ struct HutView: View {
     }
 }
 
-#Preview {
-    
-    var testHut = Hut(
-        assetID: "100033374",
-        bookable: true,
-        hutCategory: "Great Walk",
-        introduction: "This is a 54 bunk, Great Walk hut on the Kepler Track, Fiordland. Bookings required in the Great Walks season. ",
-        thumbnailURL: "https://www.doc.govt.nz/thumbs/large/link/5c957abd98084ead9568ffeb51696c65.jpg",
-        lat: -45.385232,
-        lon: 167.619159,
-        name: "Luxmore Hut",
-        staticLink: "https://www.doc.govt.nz/link/dc756fa57891438b8f3fa03813fb7260.aspx",
-        status: "OPEN",
-        x: 1178767,
-        y: 4960153
+struct HutView_Previews: PreviewProvider {
+    static var previews: some View {
+        let testHut = Hut(
+            assetID: "100033374",
+            bookable: true,
+            hutCategory: "Great Walk",
+            introduction: "This is a 54 bunk, Great Walk hut on the Kepler Track, Fiordland. Bookings required in the Great Walks season. ",
+            thumbnailURL: "https://www.doc.govt.nz/thumbs/large/link/5c957abd98084ead9568ffeb51696c65.jpg",
+            lat: -45.385232,
+            lon: 167.619159,
+            name: "Luxmore Hut",
+            staticLink: "https://www.doc.govt.nz/link/dc756fa57891438b8f3fa03813fb7260.aspx",
+            status: "OPEN",
+            x: 1178767,
+            y: 4960153
         )
-    
-    HutView(hut: testHut)
+        
+        HutView(hut: testHut)
+    }
 }
