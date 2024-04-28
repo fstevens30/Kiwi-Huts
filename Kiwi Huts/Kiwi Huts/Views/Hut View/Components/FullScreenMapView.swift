162bee3c188650d5afe135cd411b1d38ed3d6519
Flynn Stevens <flynnstevens30@gmail.com>
2024-04-29 09:56:43 +1200

    Tidied up views and frames

---
+++ Kiwi Huts/Kiwi Huts/Views/Hut View/Components/FullScreenMapView.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenMapView.swift
+//  Kiwi Huts
+//
+//  Created by Flynn Stevens on 29/04/2024.
+//
+
+import SwiftUI
+
+struct FullScreenMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FullScreenMapView()
+}
---
+++ Kiwi Huts/Kiwi Huts/Views/Hut View/Components/HutMapCard.swift
@@ -0,0 +1,18 @@
+//
+//  HutMapCard.swift
+//  Kiwi Huts
+//
+//  Created by Flynn Stevens on 29/04/2024.
+//
+
+import SwiftUI
+
+struct HutMapCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HutMapCard()
+}
