48fe9b989155a4c97946afc5dc1e0a9801095e01
Flynn Stevens <flynnstevens30@gmail.com>
2024-05-20 15:48:48 +1200

    Modified hut view for consistant images

--- Kiwi Huts/Kiwi Huts/Views/Hut View/HutView.swift
+++ Kiwi Huts/Kiwi Huts/Views/Hut View/HutView.swift
@@ -15,24 +15,25 @@ struct HutView: View {
 
         ScrollView {
             VStack {
-                HStack {
+                VStack {
                     AsyncImage(url: URL(string: hut.introductionThumbnail)) { phase in
                         if let image = phase.image {
                             image
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(maxWidth: .infinity)
+                                .frame(maxWidth: 200)
                         } else if phase.error != nil {
                             Image(systemName: "house.fill")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(maxWidth: .infinity)
+                                .frame(width: 200)
                         } else {
                             ProgressView()
                         }
                     }
                         .clipShape(RoundedRectangle(cornerRadius: 25.0))
                         .padding()
+                        .shadow(radius: 20)
                 
                 
                     VStack {
