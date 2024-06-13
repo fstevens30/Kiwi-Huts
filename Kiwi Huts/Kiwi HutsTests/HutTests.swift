ebe662607eeaaa90b862e092feb96e6e24b969c1
Flynn Stevens <flynnstevens30@gmail.com>
2024-06-13 21:01:00 +1200

    Update workflow, remove error.txt

--- Kiwi Huts/Kiwi HutsTests/HutTests.swift
+++ Kiwi Huts/Kiwi HutsTests/HutTests.swift
@@ -1,110 +1,35 @@
 //
 //  HutTests.swift
-//  Kiwi Huts
+//  Kiwi HutsTests
 //
-//  Created by Flynn Stevens on 09/05/2024.
+//  Created by Flynn Stevens on 10/06/2024.
 //
 
 import XCTest
-@testable import Kiwi_Huts
 
-class HutTests: XCTestCase {
-    
-    func testHutInitialization() {
-        let hut = Hut(
-            id: "100033374",
-            name: "Luxmore Hut",
-            status: "OPEN",
-            region: "Fiordland",
-            y: 4960153,
-            x: 1178767,
-            locationString: "Fiordland National Park",
-            numberOfBunks: 54,
-            facilities: [
-                "Cooking",
-                "Heating",
-                "Mattresses",
-                "Lighting",
-                "Toilets - flush",
-                "Water from tap - not treated, boil before use",
-                "Water supply"
-            ],
-            hutCategory: "Great Walk",
-            proximityToRoadEnd: nil,
-            bookable: true,
-            introduction: "This is a 54 bunk, Great Walk hut on the Kepler Track, Fiordland. Bookings required in the Great Walks season.",
-            introductionThumbnail: "https://www.doc.govt.nz/thumbs/large/link/262b915193334eaba5bd07f74999b664.jpg",
-            staticLink: "https://www.doc.govt.nz/link/dc756fa57891438b8f3fa03813fb7260.aspx",
-            place: "Fiordland National Park",
-            lon: 167.619159,
-            lat: -45.385232
-        )
-        
-        XCTAssertEqual(hut.id, "100033374")
-        XCTAssertEqual(hut.name, "Luxmore Hut")
-        XCTAssertEqual(hut.status, "OPEN")
-        XCTAssertEqual(hut.region, "Fiordland")
-        XCTAssertEqual(hut.y, 4960153)
-        XCTAssertEqual(hut.x, 1178767)
-        XCTAssertEqual(hut.locationString, "Fiordland National Park")
-        XCTAssertEqual(hut.numberOfBunks, 54)
-        XCTAssertEqual(hut.facilities!, [
-            "Cooking",
-            "Heating",
-            "Mattresses",
-            "Lighting",
-            "Toilets - flush",
-            "Water from tap - not treated, boil before use",
-            "Water supply"
-        ])
-        XCTAssertEqual(hut.hutCategory, "Great Walk")
-        XCTAssertEqual(hut.proximityToRoadEnd, nil)
-        XCTAssertTrue(hut.bookable)
-        XCTAssertEqual(hut.introduction, "This is a 54 bunk, Great Walk hut on the Kepler Track, Fiordland. Bookings required in the Great Walks season.")
-        XCTAssertEqual(hut.introductionThumbnail, "https://www.doc.govt.nz/thumbs/large/link/262b915193334eaba5bd07f74999b664.jpg")
-        XCTAssertEqual(hut.staticLink, "https://www.doc.govt.nz/link/dc756fa57891438b8f3fa03813fb7260.aspx")
-        XCTAssertEqual(hut.place, "Fiordland National Park")
-        XCTAssertEqual(hut.lon, 167.619159)
-        XCTAssertEqual(hut.lat, -45.385232)
-    }
-    
-    func testHutEncoding() {
-        let hut = Hut(id: "002",
-                      name: "Riverside Hut",
-                      status: "Closed",
-                      region: "Westland",
-                      y: 543.21,
-                      x: 65.43,
-                      locationString: nil,
-                      numberOfBunks: nil,
-                      facilities: nil,
-                      hutCategory: "Standard",
-                      proximityToRoadEnd: nil,
-                      bookable: false,
-                      introduction: "A closed hut by the river",
-                      introductionThumbnail: "http://example.com/riverside.jpg",
-                      staticLink: "http://example.com/riversideinfo",
-                      place: nil,
-                      lon: 171.0000,
-                      lat: -42.0000)
+final class HutTests: XCTestCase {
 
-        let encoder = JSONEncoder()
-        var encodedData = Data()
-        do {
-            encodedData = try encoder.encode(hut)
-        } catch {
-            XCTFail("Encoding failed: \(error)")
-        }
-        
-        let decoder = JSONDecoder()
-        var decodedHut: Hut?
-        do {
-            decodedHut = try decoder.decode(Hut.self, from: encodedData)
-        } catch {
-            XCTFail("Decoding failed: \(error)")
-        }
-        
-        XCTAssertEqual(decodedHut?.id, hut.id)
-        XCTAssertEqual(decodedHut?.name, hut.name)
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
